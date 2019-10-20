ba8ffaebf1f52365b9c41a354e81ae9abcaef734
Bryce Villanueva <bryce.christian@gmail.com>
2019-10-20 04:39:30 -0400

    Update motorcontroller.ino

--- motorcontroller/motorcontroller.ino
+++ motorcontroller/motorcontroller.ino
@@ -1,14 +1,14 @@
+//BeeRice was here
+
 #define L_MOTOR 5
 #define R_MOTOR 6
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.print("Hello");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   int L_POWER = 0;
   int R_POWER = 0;
   int input = 0;
@@ -33,19 +33,6 @@ void loop() {
 //  Serial.print("R_POWER is ");
 //  Serial.println(R_POWER, DEC);
 
-    
-//    if(input = 255){
-//      L_POWER = Serial.read();
-//      input = Serial.read();
-//      Serial.println(input);
-//      Serial.println("L POWER detected");
-//    }
-//    
-//    if(input != 255){
-//      R_POWER = Serial.read();
-//      Serial.println(input);
-//      Serial.println("R POWER detected");
-//    }
   analogWrite(L_MOTOR, L_POWER);
   analogWrite(R_MOTOR, R_POWER);
   
