2b69a1702775a282374a94d2faba800247dd6ea7
Kurt Wilson <kurt4wilson@gmail.com>
2019-10-20 01:16:51 +0100

    added lock beginning

--- motion_detection.py
+++ motion_detection.py
@@ -1,83 +1,132 @@
-import argparse
 import datetime
 import imutils
 import time
 import cv2
+import numpy as np
+import sys
+import time
 
-vs = cv2.VideoCapture(0)
-firstFrame = None
 
-# loop over the frames of the video
-while True:
-    # grab the current frame and initialize the occupied/unoccupied
-    # text
-    ret, frame = vs.read()
-    text = "Unoccupied"
+def current_milli_time(): return int(round(time.time() * 1000))
 
-    # if the frame could not be grabbed, then we have reached the end
-    # of the video
-    if frame is None:
-        break
 
-    # resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width=500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+class Robot():
+    FRAME_WIDTH = 400
 
-    # if the first frame is None, initialize it
-    if firstFrame is None:
-        firstFrame = gray
-        continue
-       # compute the absolute difference between the current frame and
-    # first frame
-    frameDelta = cv2.absdiff(firstFrame, gray)
-    THRESHOLD = 25
-    thresh = cv2.threshold(frameDelta, THRESHOLD, 255, cv2.THRESH_BINARY)[1]
+    def __init__(self):
+        self.vs = cv2.VideoCapture(0)
+        ret, frame = self.vs.read()
+        self.firstFrame = None
+        self.thresh = np.zeros_like(frame)
+        self.frameDelta = np.zeros_like(frame)
+        self.gray = None
+        self.status = "SEARCHING"
 
-    # dilate the thresholded image to fill in holes, then find contours
-    # on thresholded image
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    C_AREA = 1000
-    # loop over the contours
-    for c in cnts:
-        # if the contour is too small, ignore it
-        if cv2.contourArea(c) < C_AREA:
-            continue
+        self.firstLockFrame = None
+        self.locked = False
+    def updateframe(self):
+        _, self.frame = self.vs.read()
+        if self.frame is None:
+            print("Failed to read frame!")
+            return
 
-        # compute the bounding box for the contour, draw it on the frame,
-        # and update the text
-        (x, y, w, h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(frame, str(cv2.contourArea(c)), (x, y),
+        # resize the frame, convert it to grayscale, and blur it
+        self.frame = imutils.resize(self.frame, width=self.FRAME_WIDTH)
+
+    def runLoop(self):
+        self.updateframe()
+        if self.status == "SEARCHING":
+            # dont move
+            self.get_initial_target()
+
+        # draw the text and timestamp on the frame
+        cv2.putText(self.frame, "Room Status: {}".format(self.status), (10, 20),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        text = "Occupied"
+        cv2.putText(self.frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+                    (10, self.frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+        cv2.imshow("Front camera", self.frame)
+        # show the frame and record if the user presses a key
+        # cv2.imshow("Thresh", self.thresh)
+        # cv2.imshow("Frame Delta", self.frameDelta)
+        key = cv2.waitKey(1) & 0xFF
 
-    # draw the text and timestamp on the frame
-    cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-                (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+        if key == ord("q"):
+            self.cleanup()
+            sys.exit(0)
+        if (key == ord("r")):
+            self.firstFrame = self.gray
+            self.firstLockFrame = None
+            self.locked = False
+            # pass
+        if key == ord('p'):
+            key = cv2.waitKey(0)  # wait, as a pause
 
-    # show the frame and record if the user presses a key
-    cv2.imshow("Security Feed", frame)
-    cv2.imshow("Thresh", thresh)
-    cv2.imshow("Frame Delta", frameDelta)
-    key = cv2.waitKey(1) & 0xFF
+    def cleanup(self):
+        self.vs.release()
+        cv2.destroyAllWindows()
 
-    # if the `q` key is pressed, break from the lop
-    if key == ord("q"):
-        break
+    def get_initial_target(self):
+        gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-    if (key == ord("r")):
-        firstFrame = gray
-        # pass
-    if key == ord('p'):
-        key = cv2.waitKey(0) # wait, as a pause
+        # if the first frame is None, initialize it
+        if self.firstFrame is None:
+            self.firstFrame = gray
+            return
+        # compute the absolute difference between the current frame and
+        # first frame
+        frameDelta = cv2.absdiff(self.firstFrame, gray)
+        THRESHOLD = 25
+        thresh = cv2.threshold(frameDelta, THRESHOLD,
+                               255, cv2.THRESH_BINARY)[1]
+
+        # dilate the thresholded image to fill in holes, then find contours
+        # on thresholded image
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        C_AREA = 1000
+        # loop over the contours
+        for c in cnts:
+            # if the contour is too small, ignore it
+            if cv2.contourArea(c) < C_AREA:
+                continue
+
+            # compute the bounding box for the contour, draw it on the frame,
+            # and update the text
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(self.frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(self.frame, str(cv2.contourArea(c)), (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            # self.status = "MOTION"
+            # TODO: wait a few frames, and grab the image
+            if (self.firstLockFrame == None):
+                # first time we've seen something, lets wait and see
+                self.firstLockFrame = current_milli_time()
+        if (self.firstLockFrame and len(cnts) > 0 and (current_milli_time() - self.firstLockFrame) > 1000 and not self.locked):
+
+            # a whole second
+            # grab the largest frame
+            maxIndx = 0
+            maxVal = 0
+            for i, c in enumerate(cnts):
+                area = cv2.contourArea(c)
+                if area > maxVal:
+                    maxIndx = i
+                    maxVal = area
+            largestSection = cnts[maxIndx]
+            (x, y, w, h) = cv2.boundingRect(largestSection)
+
+            cv2.imshow("the item", self.frame[y:y + h, x:x + w])
+            self.locked = True
+        cv2.imshow("Frame Delta", frameDelta)
+        self.thresh = thresh
+        self.frameDelta = frameDelta
+        self.gray = gray
 
 
-# cleanup the camera and close any open windows
-vs.stop()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    robot = Robot()
+    while True:
+        robot.runLoop()
